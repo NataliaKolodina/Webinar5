f2e1a567b659f0ea580fff9c15478fdc1c2b7673
«Natalia <kolodina_melbyk@mail.ru>
2023-02-15 12:49:10 +0400

    add task2 Change Elements Array +-

---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 32: Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+Console.Clear();
+int[] arr = GetArray(7);
+Console.WriteLine();
+Change(arr);
+
+int[] GetArray(int size) // заполнили массив
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next();
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+int[] Change(int[] array)
+{
+    int[] res = array;
+    for (int i = 0; i < array.Length; i++)
+    {
+        res[i] *= (-1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
